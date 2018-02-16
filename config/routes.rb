9a6af74084c49e50133d7297830432f7b302e467
Adrian Lara <adrianblara@gmail.com>
2018-02-16 09:46:42 -0700

    namespace documentation pages with '/doc'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/aboutthisapp', to: 'aboutthisapp#index'
-  get '/*doc_page', to: 'docs#index'
+  get '/docs/*doc_page', to: 'docs#index'
 end

db01ba647ed486cc9b4a0f1a4af2d12b714c32be
Adrian Lara <adrianblara@gmail.com>
2018-02-16 23:46:33 -0700

    aboutthisapp controller updated to render JSON of required information

--- app/controllers/aboutthisapp_controller.rb
+++ app/controllers/aboutthisapp_controller.rb
@@ -1,7 +1,13 @@
 class AboutthisappController < ApplicationController
 
   def index
+    presenter = KubePresenter.new()
 
+    render json: {
+      service: presenter.services,
+      replicaset: presenter.replicaset,
+      pods: presenter.pods
+    }
   end
 
 end

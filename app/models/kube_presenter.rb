7643cac5e8086cd8fd062d18a6fa8116902d7df0
Adrian Lara <adrianblara@gmail.com>
2018-02-19 14:06:58 -0700

    Bearer token found from within Kubernetes cluster
    Note: Locally, /aboutthisapp endpoint is no longer available.

--- app/models/kube_presenter.rb
+++ app/models/kube_presenter.rb
@@ -29,7 +29,7 @@ class KubePresenter
     attr_reader :client
 
     def create_client
-      auth_options = { bearer_token: ENV['bearer_token'] }
+      auth_options = { bearer_token_file: "/var/run/secrets/kubernetes.io/serviceaccount/token" }
       ssl_options = { verify_ssl: OpenSSL::SSL::VERIFY_NONE }
 
       @client = Kubeclient::Client.new('https://192.168.99.100:8443/api/', 'v1', ssl_options: ssl_options, auth_options: auth_options)

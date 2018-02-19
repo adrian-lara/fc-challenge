383fdf7dfeb255341b0e19de833a4fea1bccf510
Adrian Lara <adrianblara@gmail.com>
2018-02-18 17:56:21 -0700

    create route for service annotation search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
+
   get '/aboutthisapp', to: 'aboutthisapp#index'
+
+  get '/service-annotation-search', to: 'annotation_search#service'
+
   get '/docs/*doc_page', to: 'docs#index'
 end
